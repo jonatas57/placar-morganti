e6778ab396034f120fd091fc9a2fbd400c833ee6
jonatas57 <jdjonatasduarte57@gmail.com>
2024-09-21 18:53:12 -0300

    feat: adiciona limite de pontuação/faltas

--- scripts/fighter.gd
+++ scripts/fighter.gd
@@ -10,17 +10,25 @@ func addLabel(label: Label):
 	labels.append(label)
 
 func increaseScore():
+	if score == MatchConfiguration.MAX_FIGHTER_SCORE:
+		return
 	score += 1
 	updateLabels()
 
 func decreaseScore():
+	if score == 0:
+		return
 	score -= 1
 	updateLabels()
 
 func giveFault():
+	if faults == MatchConfiguration.MAX_FIGHTER_FAULTS:
+		return
 	faults += 1
 
 func removeFault():
+	if faults == 0:
+		return
 	faults += 1
 
 func updateLabels():
